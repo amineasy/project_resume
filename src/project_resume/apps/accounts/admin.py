fbe31627ccd5d23d2e846269dd2de48310adcb31
amin <aminmosavi4433@gmail.com>
2025-04-09 11:42:42 +0330

    make custom user

--- src/project_resume/apps/accounts/admin.py
+++ src/project_resume/apps/accounts/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from project_resume.apps.accounts.models import CustomUser
+
+
+
+
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name",)}),
+        (_("code authorization"), {"fields": ("email_confirmed",)}),
+
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username",'email', "password1", "password2"),
+            },
+        ),
+    )
+
+
+
+
+admin.site.register(CustomUser,CustomUserAdmin)
--- src/project_resume/apps/accounts/models.py
+++ src/project_resume/apps/accounts/models.py
@@ -1,3 +1,43 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
 
-# Create your models here.
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, password, email=None, **extra_fields):
+        if not username:
+            raise ValueError('نام کاربری را وارد کنید')
+        if not password:
+            raise ValueError('رمز عبور را وارد کنید')
+        if email:
+            email = self.normalize_email(email)
+            if self.model.objects.filter(email=email).exists():
+                raise ValueError('ایمیل از قبل وجود دارد')
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+
+
+    def create_superuser(self, username, password, email=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        self.create_user(username, password, email, **extra_fields)
+
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    email_confirmed = models.BooleanField(default=False)
+
+    REQUIRED_FIELDS = []

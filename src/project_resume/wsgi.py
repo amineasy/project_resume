441064540e43b2e4811d5c5ab0ee980838bec25d
amin <aminmosavi4433@gmail.com>
2025-04-19 16:34:40 +0330

    Fix paths

--- src/project_resume/wsgi.py
+++ src/project_resume/wsgi.py
@@ -5,6 +5,6 @@ sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_resume.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.project_resume.settings')
 
 application = get_wsgi_application()

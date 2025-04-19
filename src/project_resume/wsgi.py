d115cd784d7c29cc9bc1e9e878300505000f76da
amin <aminmosavi4433@gmail.com>
2025-04-19 14:50:32 +0330

    Fix gunicorn and wsgi paths

--- src/project_resume/wsgi.py
+++ src/project_resume/wsgi.py
@@ -1,13 +1,7 @@
-"""
-WSGI config for project_resume project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 
 from django.core.wsgi import get_wsgi_application
 
